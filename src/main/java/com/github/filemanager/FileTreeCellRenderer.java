eeb02baf0ac2fd1b23ac4a994b9fe68bedbed773
Wojciech Michalik <wojciech.michalik.freelancer@googlemail.com>
2024-06-16 00:29:22 +0200

    ammending. refactoring in progress. FileTreeCellRenderer created

---
+++ src/main/java/com/github/filemanager/FileTreeCellRenderer.java
@@ -0,0 +1,51 @@
+package com.github.filemanager;
+
+import java.awt.Component;
+import java.io.File;
+
+import javax.swing.JLabel;
+import javax.swing.JTree;
+import javax.swing.filechooser.FileSystemView;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
+
+/** A TreeCellRenderer for a File. */
+class FileTreeCellRenderer extends DefaultTreeCellRenderer {
+
+    private FileSystemView fileSystemView;
+
+    private JLabel label;
+
+    FileTreeCellRenderer() {
+        label = new JLabel();
+        label.setOpaque(true);
+        fileSystemView = FileSystemView.getFileSystemView();
+    }
+
+    @Override
+    public Component getTreeCellRendererComponent(
+            JTree tree,
+            Object value,
+            boolean selected,
+            boolean expanded,
+            boolean leaf,
+            int row,
+            boolean hasFocus) {
+
+        DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
+        File file = (File) node.getUserObject();
+        label.setIcon(fileSystemView.getSystemIcon(file));
+        label.setText(fileSystemView.getSystemDisplayName(file));
+        label.setToolTipText(file.getPath());
+
+        if (selected) {
+            label.setBackground(backgroundSelectionColor);
+            label.setForeground(textSelectionColor);
+        } else {
+            label.setBackground(backgroundNonSelectionColor);
+            label.setForeground(textNonSelectionColor);
+        }
+
+        return label;
+    }//getTreeCellRendererComponent END
+}//end of FileManager class
